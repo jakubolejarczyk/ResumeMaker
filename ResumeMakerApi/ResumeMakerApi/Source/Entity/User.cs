ac39e324574de5e03a6528aec4fe16991f72ad68
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-06-30 22:10:09 +0200

    Add email column to the users entity.

--- ResumeMakerApi/ResumeMakerApi/Source/Entity/User.cs
+++ ResumeMakerApi/ResumeMakerApi/Source/Entity/User.cs
@@ -3,6 +3,7 @@
 public class User
 {
     public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
