0c8f9c9fcc1cfbc499014be4b66ddd7db4fcf904
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-06-30 23:04:07 +0200

    Add null to the EndDate column in the Experience table.

--- ResumeMakerApi/ResumeMakerApi/Source/Entity/Experience.cs
+++ ResumeMakerApi/ResumeMakerApi/Source/Entity/Experience.cs
@@ -5,7 +5,7 @@ public class Experience
     public int Id { get; set; }
     public string CompanyName { get; set; } = string.Empty;
     public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
+    public DateOnly? EndDate { get; set; }
     public string JobTitle { get; set; } = string.Empty;
     public int ResumeId { get; set; }
     public Resume Resume { get; set; } = null!;
