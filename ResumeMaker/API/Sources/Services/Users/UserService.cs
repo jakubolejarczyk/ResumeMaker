85150842a77b4bfed02518185f8bc901d1711a37
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-18 11:05:14 +0200

    Create user service without implementation

--- ResumeMaker/API/Sources/Services/Users/UserService.cs
+++ ResumeMaker/API/Sources/Services/Users/UserService.cs
@@ -1,5 +1,33 @@
-﻿namespace API.Sources.Services.Users;
+﻿using API.Sources.Repositories.Users;
+using API.Sources.Requests;
+using API.Sources.Responses;
 
-public class UserService : IUserService
+namespace API.Sources.Services.Users;
+
+public class UserService(UserRepository userRepository) : IUserService
 {
+    public CreateUserResponse CreateUser(CreateUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadUserResponse ReadUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadUsersResponse ReadUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public UpdateUserResponse UpdateUser(int id, UpdateUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DeleteUserResponse DeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
