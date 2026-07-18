69dc3938cb27c3d8ef7ba9ca4b4a84251177a912
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-18 16:33:14 +0200

    Create read user and users method

--- ResumeMaker/API/Sources/Services/Users/UserService.cs
+++ ResumeMaker/API/Sources/Services/Users/UserService.cs
@@ -38,22 +38,56 @@ public class UserService(IUserRepository userRepository) : IUserService
         };
     }
 
-    public ResponseCore<UserResponse> DeleteUser(int id)
-    {
-        throw new NotImplementedException();
-    }
-
     public ResponseCore<UserResponse> ReadUser(int id)
     {
-        throw new NotImplementedException();
+        var dto = userRepository.Read(id);
+        var body = dto.Body;
+        return new ResponseCore<UserResponse>
+        {
+            Success = dto.Success,
+            Message = dto.Message,
+            Body = body == null ? null : new UserResponse()
+            {
+                Id = body.Id,
+                Email = body.Email,
+                FirstName = body.FirstName,
+                LastName = body.LastName,
+                City = body.City,
+                Country = body.Country,
+                PhoneNumber = body.PhoneNumber
+            }
+        };
     }
 
     public ResponseCore<List<UserResponse>> ReadUsers()
     {
-        throw new NotImplementedException();
+        var dto = userRepository.ReadAll();
+        var body = dto.Body;
+        return new ResponseCore<List<UserResponse>>
+        {
+            Success = dto.Success,
+            Message = dto.Message,
+            Body = body == null ? [] : body
+                .Select(b => new UserResponse()
+                {
+                    Id = b.Id,
+                    Email = b.Email,
+                    FirstName = b.FirstName,
+                    LastName = b.LastName,
+                    City = b.City,
+                    Country = b.Country,
+                    PhoneNumber = b.PhoneNumber
+                })
+                .ToList()
+        };
     }
 
     public ResponseCore<UserResponse> UpdateUser(int id, UserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseCore<UserResponse> DeleteUser(int id)
     {
         throw new NotImplementedException();
     }
