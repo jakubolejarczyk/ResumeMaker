0a99fad409b33db343617fa6a4d89f3ab653cc7d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-19 19:52:01 +0200

    Creeate a reactive form example

--- ResumeMaker/Client/src/app/components/forms/create-user/create-user-form.component.ts
+++ ResumeMaker/Client/src/app/components/forms/create-user/create-user-form.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-create-user-form-component',
   templateUrl: './create-user-form.component.html',
   standalone: false
 })
-export class CreateUserFormComponent { }
+export class CreateUserFormComponent {
+  name = new FormControl('');
+
+  updateName() {
+    this.name.setValue('Nancy');
+  }
+}
--- ResumeMaker/Client/src/app/components/forms/forms-module.ts
+++ ResumeMaker/Client/src/app/components/forms/forms-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 
 import { CreateUserFormComponent } from "./create-user/create-user-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CreateUserFormComponent],
+  imports: [ReactiveFormsModule],
   exports: [CreateUserFormComponent]
 })
 export class FormsModule { }
