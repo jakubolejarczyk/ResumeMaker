6600cef3b161f1e16c7dcb0350df450dea28b565
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-19 21:48:58 +0200

    Create the complete create user form

--- ResumeMaker/Client/src/app/components/main/main.component.ts
+++ ResumeMaker/Client/src/app/components/main/main.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-main-component',
   templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
   standalone: false
 })
 export class MainComponent { }
--- ResumeMaker/Client/src/app/components/views/users/users-view.component.ts
+++ ResumeMaker/Client/src/app/components/views/users/users-view.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-users-view-component',
   templateUrl: './users-view.component.html',
+  styleUrl: './users-view.component.css',
   standalone: false
 })
 export class UsersViewComponent { }
