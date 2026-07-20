92404b44dd7421151e4a8ca2a9946f1f3ccc7a75
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-20 15:36:56 +0200

    Create create user controller

--- ResumeMaker/API/Program.cs
+++ ResumeMaker/API/Program.cs
@@ -4,7 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 SourcesProvider.GetBuilder(builder);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 SourcesProvider.GetApp(app);
 
--- ResumeMaker/API/Sources/Controllers/UserController.cs
+++ ResumeMaker/API/Sources/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using API.Sources.Requests;
+﻿using API.Sources.Cores;
+using API.Sources.Requests;
 using API.Sources.Responses;
 using API.Sources.Services.Users;
 using Microsoft.AspNetCore.Mvc;
@@ -9,36 +10,14 @@ namespace API.Sources.Controllers;
 [Route("api/[controller]")]
 public class UserController(IUserService service) : ControllerBase
 {
-    //[HttpPost]
-    //public async Task<ActionResult<UserResponse>> CreateUser([FromBody] UserRequest request)
-    //{
-    //    var response = await service.CreateUser(request);
-    //    if (response.Success)
-    //    {
-    //        return Ok(response);
-    //    }
-    //    return BadRequest(response);
-    //}
-
-    //[HttpGet("{id}")]
-    //public async Task<ActionResult<ReadUserResponse>> ReadUser(int id)
-    //{
-    //    var response = await service.ReadUser(id);
-    //    if (response.Success)
-    //    {
-    //        return Ok(response);
-    //    }
-    //    return NotFound(response);
-    //}
-
-    //[HttpGet]
-    //public async Task<ActionResult<ReadUsersResponse>> ReadUsers()
-    //{
-    //    var response = await service.ReadUsers();
-    //    if (response.Success)
-    //    {
-    //        return Ok(response);
-    //    }
-    //    return NotFound(response);
-    //}
+    [HttpPost]
+    public ActionResult<ResponseCore<UserResponse>> CreateUser([FromBody] UserRequest request)
+    {
+        var response = service.CreateUser(request);
+        if (response.Success)
+        {
+            return Ok(response);
+        }
+        return BadRequest(response);
+    }
 }
