4402ee183ac1f72e3a7faa7ba162a15236f6fa41
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-22 08:43:21 +0200

    Create more controls

--- ResumeMaker/Client/src/app/component/view/resumes/resumes-view.component.ts
+++ ResumeMaker/Client/src/app/component/view/resumes/resumes-view.component.ts
@@ -16,41 +16,19 @@ export class ResumesViewComponent {
     name: ['', Validators.required],
     jobTitle: ['', Validators.required],
     description: ['', Validators.required],
-    socialMedias: this.formBuilder.array([])
+    socialMedias: this.formBuilder.array([]),
+    educations: this.formBuilder.array([]),
+    experience: this.formBuilder.array([]),
+    experienceDescription: this.formBuilder.array([])
   });
 
   onSubmit() {
     console.log(this.resumeForm.value);
-
-    const body: any = this.resumeForm.value;
-    body.userId = 0;
-    body.socialMedias = body.socialMedias == null ? [] : body.socialMedias.map((i: any) => ({
-      ...i,
-      order: 0
-    }))
-
-    this.httpClient.post('http://localhost:5038/api/resume', this.resumeForm.value).subscribe(response => {
-      console.log(response);
-    });
   }
 
-  createSocialMedia() {
-    return this.formBuilder.group({
-      label: [''],
-      link: ['']
-    });
-  }
-
+  // Social medias
   getSocialMedias() {
-    return this.resumeForm.get('socialMedias') as FormArray;
-  }
-
-  addSocialMedia() {
-    this.getSocialMedias().push(this.createSocialMedia());
-  }
-
-  removeSocialMedia(index: number) {
-    this.getSocialMedias().removeAt(index);
+    return <FormArray> this.resumeForm.get('socialMedias');
   }
 
   moveSocialMedia(fromIndex: number, toIndex: number) {
@@ -69,5 +47,125 @@ export class ResumesViewComponent {
 
   moveSodialMediaDown(index: number) {
     this.moveSocialMedia(index, index + 1);
+  }
+
+  removeSocialMedia(index: number) {
+    this.getSocialMedias().removeAt(index);
+  }
+
+  addSocialMedia() {
+    const control = this.formBuilder.group({
+      label: ['', Validators.required],
+      link: ['', Validators.required]
+    });
+    this.getSocialMedias().push(control);
+  }
+
+  // Educations
+  getEducations() {
+    return <FormArray>this.resumeForm.get('educations');
+  }
+
+  moveEducation(fromIndex: number, toIndex: number) {
+    const items = this.getEducations();
+    if (toIndex < 0 || toIndex >= items.length) {
+      return;
+    }
+    const control = items.at(fromIndex);
+    items.removeAt(fromIndex);
+    items.insert(toIndex, control);
+  }
+
+  moveEducationUp(index: number) {
+    this.moveEducation(index, index - 1);
+  }
+
+  moveEducationDown(index: number) {
+    this.moveEducation(index, index + 1);
+  }
+
+  removeEducation(index: number) {
+    this.getEducations().removeAt(index);
+  }
+
+  addEducation() {
+    const control = this.formBuilder.group({
+      institutionName: ['', Validators.required],
+      fieldOfStudy: ['', Validators.required],
+      degree: ['', Validators.required],
+      graduationYear: ['', Validators.required]
+    });
+    this.getEducations().push(control);
+  }
+
+  // Experience
+  getExperience() {
+    return <FormArray>this.resumeForm.get('experience');
+  }
+
+  moveExperience(fromIndex: number, toIndex: number) {
+    const items = this.getExperience();
+    if (toIndex < 0 || toIndex >= items.length) {
+      return;
+    }
+    const control = items.at(fromIndex);
+    items.removeAt(fromIndex);
+    items.insert(toIndex, control);
+  }
+
+  moveExperienceUp(index: number) {
+    this.moveExperience(index, index - 1);
+  }
+
+  moveExperienceDown(index: number) {
+    this.moveExperience(index, index + 1);
+  }
+
+  removeExperience(index: number) {
+    this.getExperience().removeAt(index);
+  }
+
+  addExperience() {
+    const control = this.formBuilder.group({
+      companyName: ['', Validators.required],
+      jobTitle: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required]
+    });
+    this.getExperience().push(control);
+  }
+
+  // Experience description
+  getExperienceDescription() {
+    return <FormArray>this.resumeForm.get('experienceDescription');
+  }
+
+  moveExperienceDescription(fromIndex: number, toIndex: number) {
+    const items = this.getExperienceDescription();
+    if (toIndex < 0 || toIndex >= items.length) {
+      return;
+    }
+    const control = items.at(fromIndex);
+    items.removeAt(fromIndex);
+    items.insert(toIndex, control);
+  }
+
+  moveExperienceDescriptionUp(index: number) {
+    this.moveExperienceDescription(index, index - 1);
+  }
+
+  moveExperiencDescriptioneDown(index: number) {
+    this.moveExperienceDescription(index, index + 1);
+  }
+
+  removeExperienceDescription(index: number) {
+    this.getExperienceDescription().removeAt(index);
+  }
+
+  addExperienceDescription() {
+    const control = this.formBuilder.group({
+      description: ['', Validators.required]
+    });
+    this.getExperienceDescription().push(control);
   }
 }
