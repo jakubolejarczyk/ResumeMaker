b9209594bb2720067539035636e6be7074d2b7d2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-25 15:09:34 +0200

    Add update company form to the ng module

--- ResumeMaker/Client/src/app/component/form/form-module.ts
+++ ResumeMaker/Client/src/app/component/form/form-module.ts
@@ -4,18 +4,21 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { CreateCompanyFormComponent } from "./create-company/create-company-form.component";
 import { CreateUserFormComponent } from "./create-user/create-user-form.component";
 import { UpdateUserFormComponent } from "./update-user/update-user-form.component";
+import { UpdateCompanyFormComponent } from "./update-company/update-company-form.component";
 
 @NgModule({
   declarations: [
     CreateCompanyFormComponent,
     CreateUserFormComponent,
-    UpdateUserFormComponent
+    UpdateUserFormComponent,
+    UpdateCompanyFormComponent
   ],
   imports: [ReactiveFormsModule],
   exports: [
     CreateCompanyFormComponent,
     CreateUserFormComponent,
-    UpdateUserFormComponent
+    UpdateUserFormComponent,
+    UpdateCompanyFormComponent
   ]
 })
 export class FormModule {}
