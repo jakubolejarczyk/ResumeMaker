23697aec719092c5f117277517d629833be8b9fa
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-07-27 18:15:39 +0200

    Set up the ngxs in the project

--- ResumeMaker/Client/src/app/app-module.ts
+++ ResumeMaker/Client/src/app/app-module.ts
@@ -5,18 +5,19 @@ import { provideHttpClient } from '@angular/common/http';
 import { App } from './app';
 import { AppRoutingModule } from './app-routing-module';
 import { ComponentModule } from './component/component-module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [App],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentModule
+    ComponentModule,
+    NgxsModule.forRoot([], { developmentMode: /** !environment.production */ false }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideHttpClient()
-  ],
-  bootstrap: [App]
+  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
