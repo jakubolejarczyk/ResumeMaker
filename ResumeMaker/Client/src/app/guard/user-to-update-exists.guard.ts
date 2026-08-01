3a11f2480dd0078bb16a0779f3a0208f03e0701d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-01 12:32:42 +0200

    Refactor the user to update guard

--- ResumeMaker/Client/src/app/guard/user-to-update-exists.guard.ts
+++ ResumeMaker/Client/src/app/guard/user-to-update-exists.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
-import { catchError, concatMap, map, of } from "rxjs";
+import { catchError, concatMap, map, of, take } from "rxjs";
 
 import { UserService } from "../service/user.service";
 
@@ -8,21 +8,24 @@ export const userToUpdateExists: CanActivateFn = (route: ActivatedRouteSnapshot)
   const router = inject(Router);
   const userService = inject(UserService);
   return of(route.paramMap.get('id')).pipe(
-    map(param => {
-      if (!param) throw new Error('Id parameter was not defined!');
-      if (Number.isNaN(param)) throw new Error('Id parameter is not a number!');
-      return parseInt(param);
+    take(1),
+    map(paramId => {
+      if (!paramId) throw new Error('Parameter id was not defined!');
+      if (Number.isNaN(paramId)) throw new Error('Parameter id is not a number!');
+      return parseInt(paramId);
     }),
     concatMap(id => {
       return userService.getUsers$().pipe(
-        concatMap(users => {
+        take(1),
+        map(users => {
           const userExists = users.some(user => user.id === id);
-          if (userExists) return of(true);
+          if (userExists) return true;
           throw new Error('User does not exits!');
         })
       );
     }),
-    catchError(() => {
+    catchError(error => {
+      alert(error);
       return of(router.createUrlTree(['/']))
     })
   );
