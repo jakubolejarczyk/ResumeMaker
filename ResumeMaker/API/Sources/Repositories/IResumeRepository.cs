17ced3ec3820759af8cee27c01473b194f55be92
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-02 19:23:49 +0200

    Refactor the repository for resume

---
+++ ResumeMaker/API/Sources/Repositories/IResumeRepository.cs
@@ -0,0 +1,17 @@
+﻿using API.Sources.Cores;
+using API.Sources.Entities;
+
+namespace API.Sources.Repositories;
+
+public interface IResumeRepository
+{
+    ResponseCore<Resume> Create(Resume resume);
+
+    ResponseCore<Resume> Read(int id);
+
+    ResponseCore<List<Resume>> ReadAllForUser(int userId);
+
+    ResponseCore<Resume> Update(int id, Resume resume);
+
+    ResponseCore<Resume> Delete(int id);
+}
---
+++ ResumeMaker/API/Sources/Repositories/ResumeRepository.cs
@@ -0,0 +1,103 @@
+﻿using API.Sources.Contexts;
+using API.Sources.Cores;
+using API.Sources.Entities;
+
+namespace API.Sources.Repositories;
+
+public class ResumeRepository(AppDbContext context) : IResumeRepository
+{
+    public ResponseCore<Resume> Create(Resume resume)
+    {
+        context.Resumes.Add(resume);
+        context.SaveChanges();
+        return new ResponseCore<Resume>
+        {
+            Success = true,
+            Message = "The resume was created successfully.",
+            Body = resume
+        };
+    }
+
+    public ResponseCore<Resume> Read(int id)
+    {
+        var resume = context.Resumes.FirstOrDefault(r => r.Id == id);
+        if (resume == null)
+        {
+            return new ResponseCore<Resume>
+            {
+                Success = false,
+                Message = "Failed to retrieve the resume."
+            };
+        }
+        return new ResponseCore<Resume>
+        {
+            Success = true,
+            Message = "Successfully retrieved the resume.",
+            Body = resume
+        };
+    }
+
+    public ResponseCore<List<Resume>> ReadAllForUser(int userId)
+    {
+        var resumes = context.Resumes.Where(r => r.UserId == userId).ToList();
+        if (resumes.Count > 0)
+        {
+            return new ResponseCore<List<Resume>>
+            {
+                Success = true,
+                Message = $"Successfully retrieved {resumes.Count} resumes.",
+                Body = resumes
+            };
+        }
+        return new ResponseCore<List<Resume>>
+        {
+            Success = false,
+            Message = "No resumes found."
+        };
+    }
+
+    public ResponseCore<Resume> Update(int id, Resume resume)
+    {
+        var resumeById = context.Resumes.FirstOrDefault(r => r.Id == id);
+        if (resumeById == null)
+        {
+            return new ResponseCore<Resume>
+            {
+                Success = false,
+                Message = "Failed to update the resume because it does not exist."
+            };
+        }
+        resumeById.Name = resume.Name;
+        resumeById.JobTitle = resume.JobTitle;
+        resumeById.Description = resume.Description;
+        context.Resumes.Update(resumeById);
+        context.SaveChanges();
+        return new ResponseCore<Resume>
+        {
+            Success = true,
+            Message = "Successfully updated the resume.",
+            Body = resumeById
+        };
+    }
+
+    public ResponseCore<Resume> Delete(int id)
+    {
+        var resume = context.Resumes.FirstOrDefault(r => r.Id == id);
+        if (resume == null)
+        {
+            return new ResponseCore<Resume>
+            {
+                Success = false,
+                Message = $"Failed to delete resume because resume with ID {id} does not exist."
+            };
+        }
+        context.Resumes.Remove(resume);
+        context.SaveChanges();
+        return new ResponseCore<Resume>
+        {
+            Success = true,
+            Message = $"Resume with ID {id} has been deleted successfully.",
+            Body = resume
+        };
+    }
+}
