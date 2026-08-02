064b4c9f6499bfb03808fbe4bbc629e73a47ee4c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-02 15:48:48 +0200

    Add resume context to the app db context

--- ResumeMaker/API/Sources/Contexts/AppDbContext.cs
+++ ResumeMaker/API/Sources/Contexts/AppDbContext.cs
@@ -5,15 +5,21 @@ namespace API.Sources.Contexts;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    //public DbSet<Education> Educations => Set<Education>();
-    //public DbSet<Experience> Experiences => Set<Experience>();
-    //public DbSet<ExperienceDescription> ExperienceDescriptions => Set<ExperienceDescription>();
-    //public DbSet<Resume> Resumes => Set<Resume>();
-    //public DbSet<SkillElement> SkillElements => Set<SkillElement>();
-    //public DbSet<SkillGroup> SkillGroups => Set<SkillGroup>();
-    //public DbSet<SocialMedia> SocialMedias => Set<SocialMedia>();
-
     public DbSet<Company> Companies => Set<Company>();
+
+    public DbSet<Education> Educations => Set<Education>();
+    
+    public DbSet<Experience> Experiences => Set<Experience>();
+    
+    public DbSet<ExperienceDescription> ExperienceDescriptions => Set<ExperienceDescription>();
+    
+    public DbSet<Resume> Resumes => Set<Resume>();
+    
+    public DbSet<SkillElement> SkillElements => Set<SkillElement>();
+    
+    public DbSet<SkillGroup> SkillGroups => Set<SkillGroup>();
+    
+    public DbSet<SocialMedia> SocialMedias => Set<SocialMedia>();
 
     public DbSet<User> Users => Set<User>();
 }
