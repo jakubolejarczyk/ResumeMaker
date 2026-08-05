baebe895f4115b00d436ec1a35f4f65bc0df5040
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-08-05 10:47:33 +0200

    Create the resume controller

--- ResumeMaker/API/Sources/Controllers/ResumeController.cs
+++ ResumeMaker/API/Sources/Controllers/ResumeController.cs
@@ -1,46 +1,47 @@
-﻿//using API.Sources.Cores;
-//using API.Sources.Requests;
-//using API.Sources.Services.Resumes;
-//using Microsoft.AspNetCore.Mvc;
+﻿using API.Sources.Cores;
+using API.Sources.Requests;
+using API.Sources.Responses;
+using API.Sources.Services;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace API.Sources.Controllers;
+namespace API.Sources.Controllers;
 
-//[ApiController]
-//[Route("api/[controller]")]
-//public class ResumeController(IResumeService service) : ControllerBase
-//{
-//    [HttpPost]
-//    public ActionResult<ResponseCore<ResumeResponse>> CreateResume([FromBody] ResumeRequest request)
-//    {
-//        var response = service.CreateResume(request);
-//        return Ok(response);
-//    }
+[ApiController]
+[Route("api/[controller]")]
+public class ResumeController(IResumeService service) : ControllerBase
+{
+    [HttpPost]
+    public ActionResult<ResponseCore<ResumeResponse>> Create([FromBody] ResumeRequest request)
+    {
+        var response = service.Create(request);
+        return Ok(response);
+    }
 
-//    [HttpGet("{userId}")]
-//    public ActionResult<ResponseCore<ResumeResponse>> ReadResume(int userId)
-//    {
-//        var response = service.ReadResume(userId);
-//        return Ok(response);
-//    }
+    [HttpGet("{id}")]
+    public ActionResult<ResponseCore<ResumeResponse>> Read(int id)
+    {
+        var response = service.Read(id);
+        return Ok(response);
+    }
 
-//    [HttpGet("user/{userId}")]
-//    public ActionResult<ResponseCore<List<ResumeResponse>>> ReadResumes(int userId)
-//    {
-//        var response = service.ReadResumesByUserId(userId);
-//        return Ok(response);
-//    }
+    [HttpGet("user/{userId}")]
+    public ActionResult<ResponseCore<List<ResumeResponse>>> ReadAllForUser(int userId)
+    {
+        var response = service.ReadAllForUser(userId);
+        return Ok(response);
+    }
 
-//    [HttpPatch("{userId}")]
-//    public ActionResult<ResponseCore<ResumeResponse>> UpdateResumes(int userId, [FromBody] ResumeRequest request)
-//    {
-//        var response = service.UpdateResume(userId, request);
-//        return Ok(response);
-//    }
+    [HttpPatch("{id}")]
+    public ActionResult<ResponseCore<ResumeResponse>> Update(int id, [FromBody] ResumeRequest request)
+    {
+        var response = service.Update(id, request);
+        return Ok(response);
+    }
 
-//    [HttpDelete("{resumeId}")]
-//    public ActionResult<ResponseCore<ResumeResponse>> DeleteResume(int resumeId)
-//    {
-//        var response = service.DeleteResume(resumeId);
-//        return Ok(response);
-//    }
-//}
+    [HttpDelete("{id}")]
+    public ActionResult<ResponseCore<ResumeResponse>> Delete(int id)
+    {
+        var response = service.Delete(id);
+        return Ok(response);
+    }
+}
